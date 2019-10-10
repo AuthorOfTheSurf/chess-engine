680a2ef1be164b51832faea252013ccfb49d259a
Zane Kansil <zane@videoamp.com>
2019-10-09 18:53:21 -0700

    Add some failing parser tests

--- src/parser/parser.spec.ts
+++ src/parser/parser.spec.ts
@@ -1,6 +1,20 @@
 import test from 'tape';
+import { isValidMove } from './parser';
 
-test('Math test', (t) => {
-    t.equal(2 + 2, 4);
+test('Parser', (t) => {
+    test('Out of board moves', (t) => {
+        t.equals(isValidMove('z1'), [true, 'Invalid square: "z" is not a file'], 'An impossible move: outside of board');
+        t.equals(isValidMove('h9'), [true, 'Invalid square: "9" is not a rank'], 'An impossible move: outside of board');
+        t.end();
+    });
+
+    test('Pawn moves', (t) => {
+        t.equals(isValidMove('e4'), [true, null], 'A legal pawn move');
+        t.equals(isValidMove('d4'), [true, null], 'A legal pawn move');
+        t.equals(isValidMove('exd5'), [true, null], 'Pawn on e-file captures the piece on d5');
+        t.equals(isValidMove('exd6e.p.'), [true, null], 'En passat capture by pawn on e-file on d-file. Capturing pawn ends up on d6');
+        t.end();
+    });
+
     t.end();
 });
---
+++ src/parser/parser.ts
@@ -0,0 +1,11 @@
+/**
+ * Return [true, null] IFF the given chess notation string is syntactically correct
+ *
+ * Returns [false, "<error_message>"] in the case of a syntactically invalid move. The error
+ * message string should state the first syntactic error found.
+ *
+ * @param move A proposed chess move in chess notation, e.g. "Ne3"
+ */
+export function isValidMove(move: string): [boolean, string] {
+    return [false, ""];
+}

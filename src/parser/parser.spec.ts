e0257aab4de6f10e880f09a124a799d86128b60f
Zane Kansil <zane@videoamp.com>
2019-10-14 20:22:49 -0700

    isSyntacticallyValidMove captures basic pawn moves

--- src/parser/parser.spec.ts
+++ src/parser/parser.spec.ts
@@ -15,9 +15,14 @@ describe('Parser', () => {
     });
 
     describe('#isSyntacticallyValidMove', () => {
-        it('Affirms legal pawn moves', () => {
+        it('Affirms basic pawn moves', () => {
             expect(isSyntacticallyValidMove('e4')).toEqual([true, null]);
             expect(isSyntacticallyValidMove('a8')).toEqual([true, null]);
+        });
+
+        it('Rejects impossible pawn moves with reason', () => {
+            expect(isSyntacticallyValidMove('e9')).toEqual([false, 'Target square invalid: "e9"']);
+            expect(isSyntacticallyValidMove('a0')).toEqual([false, 'Target square invalid: "a0"']);
         });
     });
 });
--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -3,6 +3,7 @@ import { flatMap } from 'lodash';
 const FILES = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 const RANKS = [1, 2, 3, 4, 5, 6, 7, 8];
 const SQUARES = flatMap(FILES, (file) => RANKS.map((rank) => file + rank));
+const LENGTH_OF_SQUARE = 2; // "e4"
 
 /**
  * Return true IFF the given square is a square that exists on a standard chess board.
@@ -21,5 +22,12 @@ export function isValidSquare(square: string): boolean {
  * @param move A proposed chess move in chess notation, e.g. "Ne3"
  */
 export function isSyntacticallyValidMove(move: string): [boolean, string] {
-    return [false, ""];
+    if (move.length === LENGTH_OF_SQUARE) {
+        const squareIsValid = isValidSquare(move);
+        if (squareIsValid) {
+            return [true, null];
+        } else {
+            return [false, `Target square invalid: "${move}"`]
+        }
+    }
 }

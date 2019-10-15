627d03b89dbb989e737d3edd3d9cc252963721ce
Zane Kansil <zane@videoamp.com>
2019-10-14 19:41:50 -0700

    Replace tape with jest, comment out `isValidMove` for now

--- src/parser/parser.spec.ts
+++ src/parser/parser.spec.ts
@@ -1,23 +1,25 @@
-import test from 'tape';
-import { isValidMove, isValidSquare } from './parser';
+import { isValidSquare } from './parser';
 
-test('Parser', (t) => {
-    test('Out of board moves', (t) => {
-        t.equals(isValidSquare('z1'), false, 'An impossible move: outside of board');
-        t.equals(isValidSquare('h9'), false, 'An impossible move: outside of board');
-        t.equals(isValidSquare('a0'), false, 'An impossible move: outside of board');
-        t.equals(isValidSquare('e4'), true, 'A valid square');
-        t.equals(isValidSquare('h8'), true, 'A valid square');
-        t.end();
+describe('Parser', () => {
+    describe('#isValidSquare', () => {
+        it('correctly identifies squares as outside of the board', () => {
+            expect(isValidSquare('z1')).toEqual(false);
+            expect(isValidSquare('h9')).toEqual(false);
+            expect(isValidSquare('a0')).toEqual(false);
+        });
+
+        it('correctly identifies squares as within the board', () => {
+            expect(isValidSquare('e4')).toEqual(true);
+            expect(isValidSquare('h8')).toEqual(true);
+        });
     });
 
-    test('Pawn moves', (t) => {
-        t.equals(isValidMove('e4'), [true, null], 'A legal pawn move');
-        t.equals(isValidMove('d4'), [true, null], 'A legal pawn move');
-        t.equals(isValidMove('exd5'), [true, null], 'Pawn on e-file captures the piece on d5');
-        t.equals(isValidMove('exd6e.p.'), [true, null], 'En passat capture by pawn on e-file on d-file. Capturing pawn ends up on d6');
-        t.end();
-    });
 
-    t.end();
+    // describe('Pawn moves', (t) => {
+    //     t.equals(isValidMove('e4'), [true, null], 'A legal pawn move');
+    //     t.equals(isValidMove('d4'), [true, null], 'A legal pawn move');
+    //     t.equals(isValidMove('exd5'), [true, null], 'Pawn on e-file captures the piece on d5');
+    //     t.equals(isValidMove('exd6e.p.'), [true, null], 'En passat capture by pawn on e-file on d-file. Capturing pawn ends up on d6');
+    //     t.end();
+    // });
 });
